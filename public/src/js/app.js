9aac19c7fcdad2c4e771eaf333e224963c9b3983
kubstar <52975077+kubstar@users.noreply.github.com>
2022-12-08 21:20:45 +0530

    static caching --commit

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -9,36 +9,10 @@ if(!window.Promise){
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sw.js').then(() => {
         console.log('Service worker registered');
+    }).catch(function (err) {
+        console.log(err);
     });
 }
-
-//fetch - GET
-fetch('https://catfact.ninja/fact').then(function (response) {
-    console.log(response);
-    return response.json();
-}).then(function (data) { //promise
-    console.log(data);
-}).catch(function (err) {
-    console.log(err);
-});
-//POST
-fetch('https://httpbin.org/post', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-    },
-    mode: 'cors',
-    body: JSON.stringify({ message: 'Its works??!' })
-}).then(function (response) {
-    console.log(response);
-    return response.json();
-}).then(function (data) { //promise
-    console.log(data);
-}).catch(function (err) {
-    console.log(err);
-});
-
 
 //install-prompt banner(later)
 window.addEventListener('beforeinstallprompt', (event) => {
--- public/sw.js
+++ public/sw.js
@@ -1,16 +1,40 @@
 //installing worker
-self.addEventListener('install',(event)=>{
+self.addEventListener('install', (event) => {
     console.log('[Service-worker] installing worker...', event);
+    //wait for caching to be done
+    event.waitUntil(caches.open('static')
+        //cache
+        .then(function (cache) {
+            console.log('caching app shell');
+            cache.addAll([
+                '/',
+                '/index.html',
+                '/src/js/app.js',
+                '/src/js/material.min.js',
+                '/src/css/app.css',
+                '/src/css/feed.css',
+                '/src/images/main-image-sm.jpg',
+                'https://fonts.googleapis.com/css?family=Roboto:400,700',
+                'https://fonts.googleapis.com/icon?family=Material+Icons',
+                'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+            ]);
+        }))
 });
 
 //activating worker
-self.addEventListener('activate',(event)=>{
+self.addEventListener('activate', (event) => {
     console.log('[Service-worker] activating worker...', event);
     return self.clients.claim();
 });
 
 //fetch
-self.addEventListener('fetch', (event)=>{
-    console.log('[Service-worker] fetching...', event);
-    event.respondWith(fetch(event.request));
+self.addEventListener('fetch', (event) => {
+    event.respondWith(caches.match(event.request).then((response) => {
+        if (response) {
+            return response;
+        } else {
+            return fetch(event.request);
+        }
+    })
+    );
 });
