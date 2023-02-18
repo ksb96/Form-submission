8714d946a62b80e49a6a41b8f9430172c147587e
kubstar <52975077+kubstar@users.noreply.github.com>
2023-02-18 23:10:36 +0530

    page redesign -C

--- public/sw.js
+++ public/sw.js
@@ -9,11 +9,13 @@ self.addEventListener('install', (event) => {
             cache.addAll([
                 '/',
                 '/index.html',
+                '/about.html',
+                '/help.html',
                 '/src/js/app.js',
                 '/src/js/material.min.js',
                 '/src/css/app.css',
                 '/src/css/feed.css',
-                '/src/images/main-image-sm.jpg',
+                // '/src/images/main-image-sm.jpg',
                 'https://fonts.googleapis.com/css?family=Roboto:400,700',
                 'https://fonts.googleapis.com/icon?family=Material+Icons',
                 'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
