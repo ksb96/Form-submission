9c7f59176516f27db3beb81645459f4d63ce6940
kubstar <52975077+kubstar@users.noreply.github.com>
2023-03-09 21:05:39 +0530

    indexDB extra file -code --A

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -10,11 +10,23 @@ function closeCreatePostModal() {
   createPostArea.style.display = 'none';
 }
 
-// fetch('https://httpbin.org/get').then((res)=>{
-//   return res.json();
-// }).then((err)=>{
-//   console.log('Errrrr!!!Sorry data cannot be fetch now');
-// })
+// var url = 'https://submit-form-72a70-default-rtdb.europe-west1.firebasedatabase.app/.json';
+// var networkDataRecieve = false;
+
+fetch(url,{
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  },
+  body: JSON.stringify({
+    message: ''
+  })
+}).then((res)=>{
+  return res.json();
+}).then((err)=>{
+  console.log('Errrrr!!!Sorry data cannot be fetch now');
+})
 
 shareImageButton.addEventListener('click', openCreatePostModal);
 closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
--- public/sw.js
+++ public/sw.js
@@ -1,19 +1,19 @@
 var CACHE_STATIC_VERSION = 'static-v9';
 var CACHE_DYNAMIC_VERSION = 'dynamic-v7';
 var STATIC_FILES = [
-                    '/',
-                    '/index.html',
-                    '/src/pages/offline.html',
-                    // '/about.html',
-                    // '/help.html',
-                    '/src/js/app.js',
-                    '/src/js/material.min.js',
-                    '/src/css/app.css',
-                    '/src/css/feed.css',
-                    'https://fonts.googleapis.com/css?family=Roboto:400,700',
-                    'https://fonts.googleapis.com/icon?family=Material+Icons',
-                    'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
-                    ];
+    '/',
+    '/index.html',
+    '/src/pages/offline.html',
+    // '/about.html',
+    // '/help.html',
+    '/src/js/app.js',
+    '/src/js/material.min.js',
+    '/src/css/app.css',
+    '/src/css/feed.css',
+    'https://fonts.googleapis.com/css?family=Roboto:400,700',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+];
 
 //installing worker
 self.addEventListener('install', (event) => {
@@ -44,14 +44,14 @@ self.addEventListener('activate', (event) => {
 
 // fetch - from network
 self.addEventListener('fetch', (event) => {
-    event.respondWith(fetch(event.request).then((res)=>{
+    event.respondWith(fetch(event.request).then((res) => {
         return caches.open(CACHE_DYNAMIC_VERSION).then((cache) => {
             cache.put(event.request.url, res.clone());
             return res;
         })
-    }).catch((err)=>{ //network fetch
-       return caches.match(event.request) //cache fetch, if network fetch fail
-        })
+    }).catch((err) => { //network fetch
+        return caches.match(event.request) //cache fetch, if network fetch fail
+    })
     );
 })
 
@@ -71,11 +71,11 @@ self.addEventListener('fetch', (event) => {
                 });
         }
     }).catch((err) => {
-        return caches.open(CACHE_STATIC_VERSION).then((cache)=>{
-            if(event.request.headers.get('accept').includes('text/html')){ //fallback
+        return caches.open(CACHE_STATIC_VERSION).then((cache) => {
+            if (event.request.headers.get('accept').includes('text/html')) { //fallback
                 return cache.match('/index.html');
             }
-           
+
         });
     })
     );
