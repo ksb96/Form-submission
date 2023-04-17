cccf865738a76a008b23e40257d0b0ee2d8547e7
kubstar <52975077+kubstar@users.noreply.github.com>
2023-04-17 20:44:35 +0530

    code -U

---
+++ public/models/users.js
@@ -0,0 +1,35 @@
+// const mongoose = require('mongoose')
+
+// const UserSchema = new mongoose.Schema({
+//     _id: {
+//         type: String,
+//         required: true
+//     },
+//     name: {
+//         type: String,
+//         required: true
+//     },
+//     currentAddr: {
+//         type: String,
+//     },
+//     email: {
+//         type: String,
+//         required: true
+//     },
+//     dob: {
+//         type: Int32Array
+//     },
+//     teleNo: {
+//         type: Int32Array,
+//         required: true
+//     },
+//     textarea:{
+//         type: String
+//     },
+//     createAt: {
+//         type: Date,
+//         default: Date.now
+//     }
+// })
+
+// module.exports = mongoose.model('User', UserSchema)
--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,4 +1,6 @@
 var deferPrompt;
+// const mongoose = require('mongoose')
+// const MongoStore = require('connect-mongo');
 
 //legacy support - promise & fetch
 if(!window.Promise){
@@ -21,3 +23,13 @@ window.addEventListener('beforeinstallprompt', (event) => {
     deferPrompt = event;
     return false;
 });
+
+//mongodb connect
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://kushalcr78017:pwaTest23@pwatest.1getlb6.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
